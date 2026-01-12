31daec300577a96fe630287aa1e95fd1fbbd8a06
srbostjan <bostjanrivera@gmail.com>
2026-01-11 22:11:13 -0500

    feat: create amplify app

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,13 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 /**
- * E-Commerce Backend Configuration
- *
- * Resources:
- * - auth: Cognito authentication for CMS administrators
- * - data: AppSync GraphQL API with DynamoDB for products & analytics
- *
- * @see https://docs.amplify.aws/react/build-a-backend/
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
